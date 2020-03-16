9e0d5a30689c5698ec2eb875a9cf3e7e7f8e9f13
Kevin Florio <kflorio98@gmail.com>
2020-03-16 13:31:07 -0700

    created initial gui

--- SOAPTester/Form1.Designer.cs
+++ SOAPTester/Form1.Designer.cs
@@ -28,13 +28,175 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.EndPoint = new System.Windows.Forms.TextBox();
+            this.AddDcpBtn = new System.Windows.Forms.Button();
+            this.Preview = new System.Windows.Forms.TextBox();
+            this.Value = new System.Windows.Forms.TextBox();
+            this.Tag = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SendBtn = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.PreviewBtn = new System.Windows.Forms.Button();
+            this.Returned = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // EndPoint
+            // 
+            this.EndPoint.Location = new System.Drawing.Point(12, 27);
+            this.EndPoint.Name = "EndPoint";
+            this.EndPoint.Size = new System.Drawing.Size(279, 20);
+            this.EndPoint.TabIndex = 0;
+            // 
+            // AddDcpBtn
+            // 
+            this.AddDcpBtn.Location = new System.Drawing.Point(224, 75);
+            this.AddDcpBtn.Name = "AddDcpBtn";
+            this.AddDcpBtn.Size = new System.Drawing.Size(75, 23);
+            this.AddDcpBtn.TabIndex = 3;
+            this.AddDcpBtn.Text = "ADD DCP";
+            this.AddDcpBtn.UseVisualStyleBackColor = true;
+            this.AddDcpBtn.Click += new System.EventHandler(this.AddDcpBtn_Click);
+            // 
+            // Preview
+            // 
+            this.Preview.Location = new System.Drawing.Point(12, 145);
+            this.Preview.Multiline = true;
+            this.Preview.Name = "Preview";
+            this.Preview.ReadOnly = true;
+            this.Preview.Size = new System.Drawing.Size(380, 293);
+            this.Preview.TabIndex = 4;
+            // 
+            // Value
+            // 
+            this.Value.Location = new System.Drawing.Point(118, 78);
+            this.Value.Name = "Value";
+            this.Value.Size = new System.Drawing.Size(100, 20);
+            this.Value.TabIndex = 2;
+            // 
+            // Tag
+            // 
+            this.Tag.Location = new System.Drawing.Point(12, 78);
+            this.Tag.Name = "Tag";
+            this.Tag.Size = new System.Drawing.Size(100, 20);
+            this.Tag.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(117, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Endpoint";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(147, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Value";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Tag";
+            // 
+            // SendBtn
+            // 
+            this.SendBtn.Location = new System.Drawing.Point(674, 12);
+            this.SendBtn.Name = "SendBtn";
+            this.SendBtn.Size = new System.Drawing.Size(114, 43);
+            this.SendBtn.TabIndex = 5;
+            this.SendBtn.Text = "Send Message";
+            this.SendBtn.UseVisualStyleBackColor = true;
+            this.SendBtn.Click += new System.EventHandler(this.SendBtn_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(115, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "XML Raw Preview";
+            // 
+            // PreviewBtn
+            // 
+            this.PreviewBtn.Location = new System.Drawing.Point(301, 116);
+            this.PreviewBtn.Name = "PreviewBtn";
+            this.PreviewBtn.Size = new System.Drawing.Size(91, 23);
+            this.PreviewBtn.TabIndex = 4;
+            this.PreviewBtn.Text = "Show Preview";
+            this.PreviewBtn.UseVisualStyleBackColor = true;
+            this.PreviewBtn.Click += new System.EventHandler(this.PreviewBtn_Click);
+            // 
+            // Returned
+            // 
+            this.Returned.Location = new System.Drawing.Point(408, 145);
+            this.Returned.Multiline = true;
+            this.Returned.Name = "Returned";
+            this.Returned.ReadOnly = true;
+            this.Returned.Size = new System.Drawing.Size(380, 293);
+            this.Returned.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(561, 126);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Return Message";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Returned);
+            this.Controls.Add(this.PreviewBtn);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.SendBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Tag);
+            this.Controls.Add(this.Value);
+            this.Controls.Add(this.Preview);
+            this.Controls.Add(this.AddDcpBtn);
+            this.Controls.Add(this.EndPoint);
+            this.Name = "Form1";
+            this.Text = "SOAPTester";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox EndPoint;
+        private System.Windows.Forms.Button AddDcpBtn;
+        private System.Windows.Forms.TextBox Preview;
+        private System.Windows.Forms.TextBox Value;
+        private System.Windows.Forms.TextBox Tag;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button SendBtn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button PreviewBtn;
+        private System.Windows.Forms.TextBox Returned;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- SOAPTester/Form1.cs
+++ SOAPTester/Form1.cs
@@ -3,18 +3,218 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace SOAPTester
 {
     public partial class Form1 : Form
     {
+        public class DCP
+        {
+            public string tagNumber;
+            public string result;
+            public DCP(string tag, string val)
+            {
+                tagNumber = tag;
+                result = val;
+            }
+        }
+
+        public string CurrXML
+        {
+            get
+            {
+                return CreateFeaturesPassedMessage();
+            }
+        }
+
+        public string ReturnedXML;
+        IDictionary<string, string> dict;
+
         public Form1()
         {
             InitializeComponent();
+            dict = new Dictionary<string, string>();
         }
+
+        private void AddDcpBtn_Click(object sender, EventArgs e)
+        {
+            dict.Add(Tag.Text.ToString(), Value.Text.ToString());
+
+            MessageBox.Show($"Added -->  Tag: {Tag.Text} / Value: {Value.Text}");
+            Tag.Text = "";
+            Value.Text = "";
+        }
+
+        private void PreviewBtn_Click(object sender, EventArgs e)
+        {
+            Preview.Text = CurrXML;
+        }
+
+        private void SendBtn_Click(object sender, EventArgs e)
+        {
+            ReturnedXML = SendXMLMessage(CurrXML);
+            if (ReturnedXML == "")
+            {
+                MessageBox.Show("Error.. empty returned message..");
+                return;
+            }
+            Returned.Text = ReturnedXML;
+        }
+
+        public string CreateFeaturesPassedMessage()
+        {
+            StringBuilder xml = new StringBuilder();
+
+            //Frame of message
+            xml.Append(@"<soapenv:Envelope xmlns:soapenv=""http://schemas.xmlsoap.org/soap/envelope/"" xmlns:mes=""http://mes.health.ge.com"" xmlns:sav=""http://saveDCP.mes.health.ge.com"">");
+            xml.Append("<soapenv:Header/>");
+            xml.Append("<soapenv:Body>");
+            xml.Append("<mes:saveDCP>");
+            xml.Append("<mes:in0>");
+            xml.Append("<sav:completeOperation>0</sav:completeOperation>");
+            xml.Append("<sav:consumedParts>");
+            xml.Append("</sav:consumedParts>");
+            xml.Append("<sav:dcps>");
+
+            // Fill in dcp tags and values here
+            for (int i = 0; i < dict.Count; i++)
+            {
+                xml.Append(CreateDCP(dict.Keys.ElementAt(i), dict[dict.Keys.ElementAt(i)]));
+            }
+            // End filling dcp tags and values
+
+            xml.Append("</sav:dcps>");
+            xml.Append("<sav:operationName>CTM_Gan-SubA</sav:operationName>");
+            xml.Append("<sav:partNumber>5454001-170-SUBSA</sav:partNumber>");
+            xml.Append("<sav:partRevision>N/A</sav:partRevision>");
+            xml.Append("<sav:password>D1NPnWPE5WwnE9vatgdNzUFICLN1WA3GM6UCLN1WA3GM6U</sav:password>");
+            xml.Append("<sav:reasonCode>PASS</sav:reasonCode>");
+            xml.Append("<sav:routeName>CTM_Gan_SubA</sav:routeName>");
+            xml.Append("<sav:serialNumber>WebTestAME001</sav:serialNumber>");
+            xml.Append("<sav:userID>555333333</sav:userID>");
+            xml.Append("</mes:in0>");
+            xml.Append("</mes:saveDCP>");
+            xml.Append("</soapenv:Body>");
+            xml.Append("</soapenv:Envelope>");
+
+            return xml.ToString();
+        }
+
+        public string CreateDCP(string tag, string value)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("<sav:DCP>");
+            sb.Append($"<sav:dcpImportTag>{tag}</sav:dcpImportTag>");
+            sb.Append($"<sav:dcpValue>{value}</sav:dcpValue>");
+            sb.Append("</sav:DCP>");
+            return sb.ToString();
+        }
+
+        private string SendXMLMessage(string message)
+        {
+            string returnMessage = "";
+
+            //Calling CreateSOAPWebRequest method    
+            HttpWebRequest request = CreateSOAPWebRequest();
+
+            XmlDocument SOAPReqBody = new XmlDocument();
+            //SOAP Body Request    
+            SOAPReqBody.LoadXml(message);
+
+            using (Stream stream = request.GetRequestStream())
+            {
+                SOAPReqBody.Save(stream);
+            }
+
+            //Geting response from request    
+            using (WebResponse Serviceres = request.GetResponse())
+            {
+                using (StreamReader rd = new StreamReader(Serviceres.GetResponseStream()))
+                {
+                    //reading stream    
+                    returnMessage = rd.ReadToEnd();
+                }
+            }
+
+            return returnMessage;
+        }
+
+        public HttpWebRequest CreateSOAPWebRequest()
+        {
+            //Making Web Request    
+            HttpWebRequest Req = (HttpWebRequest)WebRequest.Create(EndPoint.Text.ToString());
+            //Content_type    
+            Req.ContentType = "text/xml;charset=\"utf-8\"";
+            Req.Accept = "text/xml";
+            //HTTP method    
+            Req.Method = "POST";
+            //return HttpWebRequest    
+            return Req;
+        }
+
+        private bool ParseReturnMessage(string message)
+        {
+            string rawData = message;
+            List<DCP> dcps = new List<DCP>();
+
+
+            if (rawData.Trim() == "")
+            {
+                return false;
+            }
+            try
+            {
+                XDocument doc = XDocument.Parse(rawData);
+                XNamespace ns = "http://mes.health.ge.com";
+                XNamespace dcpns = "http://saveDCP.mes.health.ge.com";
+                IEnumerable<XElement> responses = doc.Descendants(dcpns + "OutputDCP");
+
+                foreach (XElement response in responses)
+                {
+                    string str1 = response.Descendants(dcpns + "dcpResult").Single().Value;
+                    string str2 = response.Descendants(dcpns + "dctTag").Single().Value;
+                    dcps.Add(new DCP(str2, str1));
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+
+            // Check empty
+            if (dcps.Count == 0)
+            {
+                // No DCPS returned
+            }
+            List<DCP> badDcps = new List<DCP>();
+
+            // Check if any DCPS have a fail value
+            foreach (DCP dcpResult in dcps)
+            {
+                // Error with this tag
+                if (dcpResult.result.ToLower() != "accept")
+                {
+                    badDcps.Add(new DCP(dcpResult.tagNumber, dcpResult.result));
+                }
+            }
+
+            // If size > 0 then there was an error in return tags
+            if (badDcps.Count > 0)
+            {
+                // Notify user of errors on the tags
+                return false;
+            }
+            return true;
+        }
+
     }
 }
