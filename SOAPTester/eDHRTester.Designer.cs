d6ea6276df05513661f96fd068152261c79edacb
Kevin Florio <kflorio98@gmail.com>
2020-10-08 07:22:46 -0700

    ff

--- SOAPTester/eDHRTester.Designer.cs
+++ SOAPTester/eDHRTester.Designer.cs
@@ -475,11 +475,13 @@
             this.Unlock.Location = new System.Drawing.Point(4, 8);
             this.Unlock.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Unlock.Name = "Unlock";
+            this.Unlock.PasswordChar = '*';
             this.Unlock.Size = new System.Drawing.Size(148, 26);
             this.Unlock.TabIndex = 44;
             // 
             // UnlockBtn
             // 
+            this.UnlockBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.UnlockBtn.Location = new System.Drawing.Point(160, 8);
             this.UnlockBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.UnlockBtn.Name = "UnlockBtn";
